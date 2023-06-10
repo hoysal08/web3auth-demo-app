121bf19f721a05db4fda2384dcabf536e18523e0
hoysal08 <suraj_yahoo.com>
2023-06-10 14:31:17 +0530

    web3Auth initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,308 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Web3Auth } from "@web3auth/modal";
+import { WALLET_ADAPTERS, CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import { OpenloginAdapter, OpenloginUserInfo } from "@web3auth/openlogin-adapter";
+import { TorusWalletConnectorPlugin} from "@web3auth/torus-wallet-connector-plugin";
+import RPC from "./ethersRPC";
+import "./App.css";
+import { ethers } from "ethers";
+import { TorusParams } from "@toruslabs/torus-embed";
+
+const clientId = "BHRb2TePpTk790LT64lHyRixjbzDLJfns9Qc4gxAythLeOsasyZFy-ZnyAHiP2DuFtVv5oFkOt2Q1h_xZqQyWxA"; // get from https://dashboard.web3auth.io
 
 function App() {
+  const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
+  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(null);
+  const[torusPlugin,setTorusPlugin]=useState<TorusWalletConnectorPlugin|null>(null);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3auth = new Web3Auth({
+          clientId, 
+          web3AuthNetwork: "testnet", // mainnet, aqua, celeste, cyan or testnet
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            chainId: "0x1a4",
+            rpcTarget: "https://goerli.optimism.io",
+            blockExplorer:"https://goerli-optimism.etherscan.io/"// This is the public RPC we have added, please pass on your own endpoint while creating an app
+          },
+        });
+
+        const openloginAdapter = new OpenloginAdapter({
+          loginSettings: {
+            mfaLevel: "default", // Pass on the mfa level of your choice: default, optional, mandatory, none
+          },
+          adapterSettings: { 
+            loginConfig: {
+              // Add login configs corresponding to the provider
+              // Google login
+              // google: {
+              //   name: "Google Login", // The desired name you want to show on the login button
+              //   verifier: "YOUR_GOOGLE_VERIFIER_NAME", // Please create a verifier on the developer dashboard and pass the name here
+              //   typeOfLogin: "google", // Pass on the login provider of the verifier you've created
+              //   clientId: "GOOGLE_CLIENT_ID.apps.googleusercontent.com", // use your app client id you got from google
+              // },
+              // Add other login providers here
+            },
+          }
+        });
+        web3auth.configureAdapter(openloginAdapter);
+        setWeb3auth(web3auth);
+
+        await web3auth.initModal();
+
+        if (web3auth.provider) {
+          setProvider(web3auth.provider);
+        };
+
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    init();
+  }, []);
+  
+  useEffect(()=>{
+
+    const initTorusWallet=async()=>{
+      console.log("Torus init called")
+      if(web3auth && provider && !torusPlugin)
+      {
+         const torusPlugin = new TorusWalletConnectorPlugin({
+           torusWalletOpts:{},
+           walletInitOptions:{
+             whiteLabel: {
+               theme: { isDark: true, colors: { primary: "#00a8ff" } },
+               logoDark: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+               logoLight: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
+             },
+             useWalletConnect: true,
+             enableLogging: true,
+           }
+         });
+         setTorusPlugin(torusPlugin);
+         await web3auth.addPlugin(torusPlugin);
+      }
+     }
+    
+    initTorusWallet();
+  },[provider,web3auth])
+
+
+  const login = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const web3authProvider = await web3auth.connect();
+    setProvider(web3authProvider);
+  };
+
+  const authenticateUser = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const idToken = await web3auth.authenticateUser();
+    uiConsole(idToken);
+  };
+
+  const getUserInfo = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const user = await web3auth.getUserInfo();
+    uiConsole(user);
+  };
+
+  const logout = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    await web3auth.logout();
+    setProvider(null);
+  };
+
+  const getChainId = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const chainId = await rpc.getChainId();
+    uiConsole(chainId);
+  };
+  const getAccounts = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const address = await rpc.getAccounts();
+    uiConsole(address);
+  };
+
+  const getBalance = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const balance = await rpc.getBalance();
+    uiConsole(balance);
+  };
+
+  const sendTransaction = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const receipt = await rpc.sendTransaction();
+    uiConsole(receipt);
+  };
+
+  const signMessage = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const signedMessage = await rpc.signMessage();
+    uiConsole(signedMessage);
+  };
+
+  const getPrivateKey = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const privateKey = await rpc.getPrivateKey();
+    uiConsole(privateKey);
+  };
+
+  const showWCM = async () => {
+    if (!torusPlugin) {
+      uiConsole("torus plugin not initialized yet");
+      return;
+    }
+    torusPlugin.showWalletConnectScanner();
+    uiConsole();
+  };
+
+  const initiateTopUp = async () => {
+    if (!torusPlugin) {
+      uiConsole("torus plugin not initialized yet");
+      return;
+    }
+    torusPlugin.initiateTopup("moonpay", {
+      selectedAddress: "0x8cFa648eBfD5736127BbaBd1d3cAe221B45AB9AF",
+      selectedCurrency: "USD",
+      fiatValue: 100,
+      selectedCryptoCurrency: "ETH",
+      chainNetwork: "mainnet",
+    });
+  };
+
+  function uiConsole(...args: any[]): void {
+    const el = document.querySelector("#console>p");
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+    }
+  }
+
+  const loggedInView = (
+    <>
+      <div className="flex-container">
+        <div>
+          <button onClick={getUserInfo} className="card">
+            Get User Info
+          </button>
+        </div>
+        <div>
+          <button onClick={authenticateUser} className="card">
+            Get ID Token
+          </button>
+        </div>
+        <div>
+          <button onClick={getChainId} className="card">
+            Get Chain ID
+          </button>
+        </div>
+        <div>
+          <button onClick={getAccounts} className="card">
+            Get Accounts
+          </button>
+        </div>
+        <div>
+          <button onClick={getBalance} className="card">
+            Get Balance
+          </button>
+        </div>
+        <div>
+          <button onClick={signMessage} className="card">
+            Sign Message
+          </button>
+        </div>
+        <div>
+          <button onClick={sendTransaction} className="card">
+            Send Transaction
+          </button>
+        </div>
+        <div>
+          <button onClick={getPrivateKey} className="card">
+            Get Private Key
+          </button>
+        </div>
+        <div>
+          <button onClick={logout} className="card">
+            Log Out
+          </button>
+        </div>
+        <div>
+          <button onClick={showWCM} className="card">
+            Show Scanner
+          </button>
+        </div>
+        <div>
+          <button onClick={initiateTopUp} className="card">
+            TopUp
+          </button>
+        </div>
+      </div>
+
+      <div id="console" style={{ whiteSpace: "pre-line" }}>
+        <p style={{ whiteSpace: "pre-line" }}>Logged in Successfully!</p>
+      </div>
+    </>
+  );
+
+  const unloggedInView = (
+    <button onClick={login} className="card">
+      Login
+    </button>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container">
+      <h1 className="title">
+        <a target="_blank" href="http://web3auth.io/" rel="noreferrer">
+          Web3Auth
         </a>
-      </header>
+        & ReactJS Example
+      </h1>
+
+      <div className="grid">{provider ? loggedInView : unloggedInView}</div>
+
+      <footer className="footer">
+        <a href="https://github.com/Web3Auth/web3auth-pnp-examples/" target="_blank" rel="noopener noreferrer">
+          Source code
+        </a>
+      </footer>
     </div>
   );
 }
